c25a54d19d939340b07122c1ea911cacea53a964
nikunj.khandelwal <nikunj.khandelwal@accenture.com>
2016-08-25 14:18:53 +0530

    js update

--- WebContent/resources/js/searchResults.js
+++ WebContent/resources/js/searchResults.js
@@ -2,7 +2,7 @@
  * 
  */
 function updateCartValue(obj){
-	obj = [{"3":{"PRODUCT_DESC":"The smooth, continuous unibody design created for iPhone 6 comes to iPhone 6s. Its familiar form feels just right in your hand. And now there are four stunning ?nishes to choose from: gold, silver, space grey, and the new rose gold.","ATTRIB_MAP":{"Brand":"Hipster","Price":"1245","Size":"8 D","Color":"Navy","ASIN":"B01D6R0OUE"},"CATEGORY_NAME":"Shoes","PRODUCT_NAME":"Apple iPhone 6s"}}];
+	//obj = [{"3":{"PRODUCT_DESC":"The smooth, continuous unibody design created for iPhone 6 comes to iPhone 6s. Its familiar form feels just right in your hand. And now there are four stunning ?nishes to choose from: gold, silver, space grey, and the new rose gold.","ATTRIB_MAP":{"Brand":"Hipster","Price":"1245","Size":"8 D","Color":"Navy","ASIN":"B01D6R0OUE"},"CATEGORY_NAME":"Shoes","PRODUCT_NAME":"Apple iPhone 6s"}}];
 	
 	var cartValue = localStorage.getItem('cartData');
 	if(typeof cartValue != "undefied" && cartValue != null && cartValue != ""){
